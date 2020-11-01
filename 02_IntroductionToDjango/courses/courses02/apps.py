77ad3cd30cbceee19e61bb8057f8ba4f19226d65
andrey <88stashew@icloud.com>
2020-11-02 00:05:52 +0300

    второе дз

---
+++ 02_IntroductionToDjango/courses/courses02/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Courses02Config(AppConfig):
+    name = 'courses02'
---
+++ 02_IntroductionToDjango/project02/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrey/PycharmProjects/python_django/02_IntroductionToDjango/project02/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
